71ea6cee22fbad60da4f7d0b987ef6be4013906d
Beniamin <106469071+redve-dev@users.noreply.github.com>
2022-11-22 09:43:10 +0100

    refactor code

--- src/main.c
+++ src/main.c
@@ -1,34 +1,46 @@
 #include <gtk/gtk.h>
 
-void print_on_entry(GtkWidget* input){
-	int len = gtk_entry_get_text_length( GTK_ENTRY( input) );
-	gtk_editable_delete_text(GTK_EDITABLE( input), 0, len);
-	gtk_editable_insert_text(GTK_EDITABLE( input ), "Testa", 5, &len);
+typedef struct{
+	GtkWidget* w1;
+	GtkWidget* w2;
+} widgets_payload;
+
+void print_on_entry(widgets_payload* data){
+	GtkWidget* input = data->w1;
+	GtkWidget* bar = data->w2;
+	const gchar* text = gtk_entry_get_text(GTK_ENTRY(input));
+	printf("%s\n", text);
+	gtk_level_bar_set_value(GTK_LEVEL_BAR(bar), 4/8.);
 }
 
 GtkWidget* create_window(GtkApplication* app){
 	GtkWidget* window = gtk_application_window_new(app);
-	gtk_window_set_title(GTK_WINDOW(window), "Hello World window");
+	gtk_window_set_title(GTK_WINDOW(window), "Password Checker");
 	gtk_window_set_default_size( GTK_WINDOW(window), 400, 200);
 	return window;
 }
 
+void connect_entry_with_bar(GtkWidget* entry, GtkWidget* bar){
+	widgets_payload* w = malloc(sizeof(widgets_payload));
+	w->w1 = entry;
+	w->w2 = bar;
+	g_signal_connect_swapped(entry, "insert-text", G_CALLBACK(print_on_entry), w);
+	g_signal_connect_swapped(entry, "delete-text", G_CALLBACK(print_on_entry), w);
+
+}
+
 static void activate(GtkApplication* app, gpointer user_data){
 	GtkWidget* window = create_window(app);
-	GtkWidget* box = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
 	GtkWidget* grid = gtk_grid_new();
-	gtk_container_add ( GTK_CONTAINER(window), box);
-	gtk_container_add(GTK_CONTAINER(box), grid);
-
-	GtkWidget* label = gtk_label_new("Hello");
-	gtk_grid_attach( GTK_GRID(grid), label, 1, 0, 1 ,1);
+	gtk_container_add(GTK_CONTAINER(window), grid);
 
 	GtkWidget* input = 	gtk_entry_new();
+	gtk_entry_set_visibility(GTK_ENTRY( input ), FALSE);
 	gtk_grid_attach( GTK_GRID(grid), input, 0, 0, 1 ,1);
 
-	GtkWidget* button = gtk_button_new_with_label("Convert");
-	g_signal_connect_swapped(button, "clicked", G_CALLBACK(print_on_entry), input);
-	gtk_grid_attach(GTK_GRID(grid), button, 0, 1, 2, 2);
+	GtkWidget* bar = gtk_level_bar_new();
+	gtk_grid_attach(GTK_GRID(grid), bar, 0, 1, 1, 1);
+	connect_entry_with_bar(input, bar);
 
 	gtk_widget_show_all(window);
 }
